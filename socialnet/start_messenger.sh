005615d73675a3b2c094376a0e8eee162b057466
natalia <Natalia Suchkova>
2023-04-23 16:33:29 +0000

    MINOR

--- socialnet/start_messenger.sh
+++ socialnet/start_messenger.sh
@@ -1,8 +1,8 @@
 export DB_HOST=127.0.0.1
 export DB_PORT=3360
-export DB_LOGIN=stud
-export DB_PASSWORD=stud
-export DB_DATABASE=archdb
+export DB_LOGIN=snetuser
+export DB_PASSWORD=snetuser
+export DB_DATABASE=snetdb
 
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
